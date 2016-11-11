65d4c68495ab606c7e3fbd120b44f4310b4fce0b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-11 12:51:27 -0800

    Install MySQL

--- provision/shell/magento.sh
+++ provision/shell/magento.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+MYSQL_ROOT_PASSWORD="password"
+DB_USER="dbuser"
+DB_PASSWORD="dbpassword"
+
+set -e
+
+apt -qqy update
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+apt install -qqy mysql-server
+
