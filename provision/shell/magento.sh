a2a784e9a79d985b144efdce30c795e1209e4905
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-11 15:18:43 -0800

    Cleaned up PHP deps

--- provision/shell/magento.sh
+++ provision/shell/magento.sh
@@ -16,5 +16,16 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -qqy mysql-server
 
 # PHP and dependencies (deps pulled from Magento docs)
-apt-get install -qqy php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv
+apt-get install -qqy \
+        php7.0 \
+        php7.0-curl \
+        php7.0-gd \
+        php7.0-intl \
+        php7.0-mbstring \
+        php7.0-mcrypt \
+        php7.0-xml \
+        php7.0-zip \
+        libapache2-mod-php7.0 \
+        composer
+
 a2enmod php7.0
