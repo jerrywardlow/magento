98949e65aa4148922f32c4def84f476be22bad16
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-13 14:04:54 -0800

    Compress public/private CIDR vars

--- aws/variables.tf
+++ aws/variables.tf
@@ -6,34 +6,21 @@ variable "region" {
 variable "vpc_cidr" {
     description = "Main CIDR block"
     default = "10.10.0.0/16"
+
+variable "public_cidr" {
+    description = "CIDR blocks for public subnets"
+    default = {
+        "0" = "10.10.1.0/24"
+        "1" = "10.10.2.0/24"
+        "2" = "10.10.3.0/24"
+    }
 }
 
-variable "pub_cidr1" {
-    description = "CIDR block for public subnet 1"
-    default = "10.10.1.0/24"
-}
-
-variable "pub_cidr2" {
-    description = "CIDR block for public subnet 2"
-    default = "10.10.2.0/24"
-}
-
-variable "pub_cidr3" {
-    description = "CIDR block for public subnet 3"
-    default = "10.10.3.0/24"
-}
-
-variable "priv_cidr1" {
-    description = "CIDR block for private subnet 1"
-    default = "10.10.101.0/24"
-}
-
-variable "priv_cidr2" {
-    description = "CIDR block for private subnet 2"
-    default = "10.10.102.0/24"
-}
-
-variable "priv_cidr3" {
-    description = "CIDR block for private subnet 3"
-    default = "10.10.103.0/24"
+variable "private_cidr" {
+    description = "CIDR blocks for private subnets"
+    default = {
+        "0" = "10.10.101.0/24"
+        "1" = "10.10.102.0/24"
+        "2" = "10.10.103.0/24"
+    }
 }
