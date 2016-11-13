afbe650c4639740bfedeb9e02d8a609161944fb8
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-13 13:35:57 -0800

    CIDR blocks for public and private subnets

--- aws/variables.tf
+++ aws/variables.tf
@@ -7,3 +7,33 @@ variable "vpc_cidr" {
     description = "Main CIDR block"
     default = "10.10.0.0/16"
 }
+
+variable "pub_cidr1" {
+    description = "CIDR block for public subnet 1"
+    default = "10.10.1.0/24"
+}
+
+variable "pub_cidr2" {
+    description = "CIDR block for public subnet 2"
+    default = "10.10.2.0/24"
+}
+
+variable "pub_cidr3" {
+    description = "CIDR block for public subnet 3"
+    default = "10.10.3.0/24"
+}
+
+variable "priv_cidr1" {
+    description = "CIDR block for private subnet 1"
+    default = "10.10.101.0/24"
+}
+
+variable "priv_cidr2" {
+    description = "CIDR block for private subnet 2"
+    default = "10.10.102.0/24"
+}
+
+variable "priv_cidr3" {
+    description = "CIDR block for private subnet 3"
+    default = "10.10.103.0/24"
+}
