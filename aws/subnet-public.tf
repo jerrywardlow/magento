1f139456d2b34bc5ef9f525e327ab534f92eac7b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-13 14:22:55 -0800

    Create route table and associate with subnets

--- aws/subnet-public.tf
+++ aws/subnet-public.tf
@@ -15,3 +15,18 @@ resource "aws_subnet" "public" {
         Name = "public-${count.index}"
     }
 }
+
+# Routing table and association
+resource "aws_route_table" "public" {
+    vpc_id = "${aws_vpc.default.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.default.id}"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count = 3
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+    route_table_id = "${aws_route_table.public.id}"
+}
