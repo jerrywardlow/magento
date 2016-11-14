9291bea6d9d63d274e3a99f66ec5bb8ef2dffdaf
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-14 14:54:14 -0800

    Basic ELB config

---
+++ aws/elb.tf
@@ -0,0 +1,23 @@
+# Load balancer
+resource "aws_elb" "app" {
+    name = "magento-app-elb"
+    subnets = ["${aws_subnet.public.*.id}"]
+    security_groups = ["pass"]
+
+    listener {
+      instance_port = 80
+      instance_protocol = "http"
+      lb_port = 80
+      lb_protocol = "http"
+    }
+
+    health_check {
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+      timeout = 3
+      target = "HTTP:80/"
+      interval = 30
+    }
+
+    instances = ["pass"]
+}
