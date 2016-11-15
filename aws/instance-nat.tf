75ae61ee8e00fe9d718065659b369c4fe4b1713b
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-14 16:37:32 -0800

    NAT for each AZ

---
+++ aws/instance-nat.tf
@@ -0,0 +1,24 @@
+# Elastic IP for NAT
+resource "aws_eip" "nat" {
+    count = 3
+    vpc = true
+}
+
+resource "aws_instance" "nat" {
+    count = 3
+    ami = "${var.nat-ami}"
+    instance_type = "t2.micro"
+    subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+    vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+    source_dest_check = false
+    key_name = "${aws_key_pair.magento.key_name}"
+    tags = {
+        Name = "magento-nat-${count.index}"
+    }
+}
+
+resource "aws_ip_association" "eip_assoc" {
+    count = 3
+    instance_id = "${element(aws_instance.nat.*.id, count.index)}"
+    allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+}
