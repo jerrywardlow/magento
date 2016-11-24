4355fe7e8260835e9dfb8d405f7c6ac894089917
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-24 14:58:29 -0800

    PHP 7 and dependencies

--- provision/shell/macentos.sh
+++ provision/shell/macentos.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Repository
+yum install -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-14.ius.centos7.noarch.rpm
+
+yum -y update
+
 
 # Apache
 yum install -y httpd
 
 systemctl start httpd.service
-
 systemctl enable httpd.service
 
 
@@ -13,9 +17,22 @@ systemctl enable httpd.service
 yum install -y mariadb-server mariadb
 
 systemctl start mariadb.service
-
 systemctl enable mariadb.service
 
 
 # PHP
-# pdo_mysql DOM simplexml iconv mcrypt curl hash SOAP GD
+yum -y install \
+    php70u \
+    php70u-pdo \
+    php70u-mysqlnd \
+    php70u-opcache \
+    php70u-xml \
+    php70u-mcrypt \
+    php70u-gd \
+    php70u-devel \
+    php70u-mysql \
+    php70u-intl \
+    php70u-mbstring \
+    php70u-bcmath \
+    php70u-json \
+    php70u-iconv
