0f771c07f9d42a23f973df1482bb05eb821f638d
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-23 17:53:15 -0800

    Apache conf

--- provision/shell/macentos.sh
+++ provision/shell/macentos.sh
@@ -3,10 +3,15 @@
 
 # Apache
 yum install -y httpd
-
 systemctl start httpd.service
-
 systemctl enable httpd.service
+
+mkdir -p /etc/httpd/sites-available
+mkdir -p /etc/httpd/sites-enabled
+
+echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf
+
+mkdir -p /var/www/magento
 
 
 # MariaDB
