66bc520bd46b9d5df9d775faa38a43775a6a6e3d
Jerry Wardlow <jerry@jerryoftheyear.com>
2016-11-23 17:44:44 -0800

    Notes

--- provision/shell/macentos.sh
+++ provision/shell/macentos.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+# Apache
 yum install -y httpd
 
 systemctl start httpd.service
@@ -7,8 +9,13 @@ systemctl start httpd.service
 systemctl enable httpd.service
 
 
+# MariaDB
 yum install -y mariadb-server mariadb
 
 systemctl start mariadb.service
 
 systemctl enable mariadb.service
+
+
+# PHP
+# pdo_mysql DOM simplexml iconv mcrypt curl hash SOAP GD
