817281d7bdcb556baabb0dc67f7192613e79b2ad
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-28 10:27:27 -0700

    Fill out cache vars

---
+++ aws/var-cache.tf
@@ -0,0 +1,15 @@
+variable "elasticache-cluster-id" {
+    description = "Cluster ID for Elasticache cluster"
+    default = "mage-redis"
+}
+
+variable "elasticache-node-type" {
+    description = "Node type for ElastiCache cluster"
+    default = "cache.t2.micro"
+}
+
+variable "elasticache-port" {
+    description = "Default ElastiCache port"
+    default = 6379
+}
+
