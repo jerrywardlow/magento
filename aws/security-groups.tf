3ba1cd646c822adae0a8ed47302db2fa0b9721a7
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-28 11:01:15 -0700

    Include NAT ssh access

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -38,6 +38,13 @@ resource "aws_security_group" "nat" {
     vpc_id = "${aws_vpc.default.id}"
 
     ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = "${var.ssh-access-list}"
+    }
+
+    ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
