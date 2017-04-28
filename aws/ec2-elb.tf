1322fd10dcdb00ed518d76e36271c017326265a6
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-28 09:26:56 -0700

    Adjust ELB health check

--- aws/ec2-elb.tf
+++ aws/ec2-elb.tf
@@ -13,10 +13,10 @@ resource "aws_elb" "app" {
 
     health_check {
       healthy_threshold = 2
-      unhealthy_threshold = 2
-      timeout = 3
+      unhealthy_threshold = 5
+      timeout = 5
       target = "HTTP:80/"
-      interval = 30
+      interval = 10
     }
 
     tags {
