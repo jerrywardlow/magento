375c0d3aeb6e59d6f5c3335d77524143f58487fb
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-28 10:14:35 -0700

    RDS instance

---
+++ aws/rds-mysql.tf
@@ -0,0 +1,32 @@
+# RDS
+resource "aws_db_instance" "mysql" {
+    identifier = "${var.rds-identifier}"
+    instance_class = "${var.rds-instance-class}" 
+    allocated_storage = "${var.rds-allocated-storage}" 
+    storage_type = "${var.rds-storage-type}"
+    engine = "mysql"
+    multi_az = "${var.rds-multi-az}"
+
+    port = "${var.rds-port}"
+
+    name = "${var.rds-db-name}"
+    username = "${var.rds-db-user}"
+    password = "${var.rds-db-password}"
+
+    vpc_security_group_ids = ["${aws_security_group.mysql.id}"]
+    db_subnet_group_name = "${aws_db_subnet_group.default.name}"
+    
+    tags {
+        group = "mage-rds"
+    }
+}
+
+# RDS subnet group
+resource "aws_db_subnet_group" "default" {
+    name = "main"
+    description = "Subnet group for RDS/MySQL"
+    subnet_ids = ["${aws_subnet.private.*.id}"]
+    tags {
+        Name = "mage-db-subnet-group"
+    }
+}
