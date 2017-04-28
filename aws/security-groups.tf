cb2151051b1d408e254774d4d61c618f4991c65c
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-28 11:03:05 -0700

    App security group

--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -70,3 +70,43 @@ resource "aws_security_group" "nat" {
         group = "mage-sg"
     }
 }
+
+# Application security group
+resource "aws_security_group" "app_asg" {
+    name = "mage-app-asg"
+    description = "Security group for application ASG"
+    vpc_id = "${aws_vpc.default.id}"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.nat.id}"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.elb.id}"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.elb.id}"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        Name = "magento-app-asg"
+        groups = "mage-sg"
+    }
+}
